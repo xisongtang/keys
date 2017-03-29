7ceb1d825df8a882486348d016a53e694dfb7d70
unknown <>
2017-03-30 01:21:54 +0800

    first commit

---
+++ src/components/KeyBody.tsx
@@ -0,0 +1,15 @@
+﻿import * as React from "react";
+
+export interface KeyBodyProps { website: string; accountId: string; }
+
+export class KeyBody extends React.Component<KeyBodyProps, undefined> {
+    render() {
+        return <div>
+            <div><span>网站</span><input type="text" value={this.props.website} /></div>
+            <div><span>账号</span><input type="text" value={this.props.accountId} /></div>
+            <div><span>密钥</span><input type="password" /></div>
+            <div><span>hash方式</span><input type="text" /></div>
+            <div><span>生成密码</span><span></span></div>
+        </div>;
+    }
+}
