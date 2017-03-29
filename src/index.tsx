a213b22f6635a47f03bd06c055cfeaf6c7915594
xisongtang <xisong.tang@qq.com>
2017-03-30 01:21:54 +0800

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+﻿import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+import { KeyBody } from "./components/KeyBody";
+
+ReactDOM.render(
+    <KeyBody website="QQ" accountId="313193401@qq.com" />,
+    document.getElementById("react-body")
+);
