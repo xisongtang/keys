2f3a174ad891233b8749c6b6aeb01a9aadd08e0f
unknown <>
2017-04-02 01:16:57 +0800

    basic function finished

--- src/components/KeyBody.tsx
+++ src/components/KeyBody.tsx
@@ -4,33 +4,47 @@ import * as CryptoJS from "crypto-js";
 interface KeyBodyProps {
     readonly website?: string;
     readonly accountId?: string;
-    readonly hashType?: string;
     readonly password?: string;
+    readonly hashType?: string;
+    readonly digitNumber?: number;
 }
 
-interface KeyBodyStates extends KeyBodyProps {
-    readonly genPassword?: string;
+interface KeyBodyStates {
+    website?: string;
+    accountId?: string;
+    password: string;
+    hashType: string;
+    digitNumber: number;
+    genPassword?: string;
+    passwordVisible: boolean;
 }
 
-let HashType: ReadonlyArray<string> = ["md5", "sha1"];
+const HASH_TYPE: ReadonlyArray<string> = ["MD5", "SHA1", "SHA256", "SHA224", "SHA384", "SHA512", "RIPEMD160"];
+
+const HASHER: Readonly<{ [key: string]: CryptoJS.lib.Hasher; }> = { MD5: CryptoJS.algo.MD5, SHA1: CryptoJS.algo.SHA1, SHA256: CryptoJS.algo.SHA256, SHA224: CryptoJS.algo.SHA224, SHA384: CryptoJS.algo.SHA384, SHA512: CryptoJS.algo.SHA512, RIPEMD160: CryptoJS.algo.RIPEMD160 };
+
+const ALPHABET: Readonly<string> = "0123456789012345678901234567890123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`~!@#$%^&*()_+-={}|<>,.{}[];:'\"\\";
 
 
 export class KeyBody extends React.Component<KeyBodyProps, KeyBodyStates> {
-    public static defaultProps: Partial<KeyBodyProps> = {
-        hashType: HashType[0]
-    }
 
     constructor(props: KeyBodyProps) {
         super(props);
         this.state = {
             website: props.website,
             accountId: props.accountId,
-            hashType: props.hashType
+            hashType: props.hashType ? props.hashType : HASH_TYPE[0],
+            digitNumber: props.digitNumber ? props.digitNumber : 16,
+            password: props.password ? props.password : 'password',
+            passwordVisible: false
         };
         this.onWebsiteChange = this.onWebsiteChange.bind(this);
         this.onAccountIdChange = this.onAccountIdChange.bind(this);
+        this.onDigitNumberChange = this.onDigitNumberChange.bind(this);
         this.onHashTypeChange = this.onHashTypeChange.bind(this);
+        this.onPasswordChange = this.onPasswordChange.bind(this);
         this.onGenerateButtonClick = this.onGenerateButtonClick.bind(this);
+        this.onToggleVisibleButtonClick = this.onToggleVisibleButtonClick.bind(this);
     }
     render() {
         return <div>
@@ -40,7 +54,7 @@ export class KeyBody extends React.Component<KeyBodyProps, KeyBodyStates> {
                 <span>hash方式</span>
                 <select value={this.state.hashType} onChange={this.onHashTypeChange}>
                     {
-                        HashType.map((type) => {
+                        HASH_TYPE.map((type) => {
                             return <option key={type} value={type}>
                                 {type.toLocaleUpperCase()}
                             </option>
@@ -48,9 +62,10 @@ export class KeyBody extends React.Component<KeyBodyProps, KeyBodyStates> {
                     }
                 </select>
             </div>
-            <div><span>密钥</span><input type="password" /></div>
+            <div><span>位数</span><input type="digitNumber" value={this.state.digitNumber} onChange={this.onDigitNumberChange} /></div>
+            <div><span>密钥</span><input type={this.state.passwordVisible ? "text" : "password"} value={this.state.password} onChange={this.onPasswordChange} /><button onClick={this.onToggleVisibleButtonClick}><span className="glyphicons x05 glyphicons-eye-open" aria-hidden="true"></span></button></div>
             <div><span>生成密码</span><span>{this.state.genPassword}</span></div>
-            <button onClick={this.onGenerateButtonClick}>生成</button>
+            <input type="button" onClick={this.onGenerateButtonClick} value="生成" />
         </div>;
     }
 
@@ -68,12 +83,40 @@ export class KeyBody extends React.Component<KeyBodyProps, KeyBodyStates> {
 
     onHashTypeChange(event: React.ChangeEvent<HTMLSelectElement>) {
         this.setState({
-            accountId: event.target.value
+            hashType: event.target.value
         })
     }
 
-    onGenerateButtonClick(event: React.MouseEvent<HTMLButtonElement>) {
-        let result: string = CryptoJS.AES.encrypt(this.state.accountId ? this.state.accountId : '', this.state.password ? this.state.password : '', { mode: CryptoJS.mode.ECB, padding: CryptoJS.pad.NoPadding }).ciphertext.toString();
-        console.log(result);
+    onDigitNumberChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            digitNumber: ~~event.target.value
+        })
+    }
+
+    onPasswordChange(event: React.ChangeEvent<HTMLInputElement>) {
+        this.setState({
+            password: event.target.value
+        })
+    }
+
+    onToggleVisibleButtonClick(event: React.MouseEvent<HTMLButtonElement>) {
+        this.setState((prevState: KeyBodyStates) => {
+            return { passwordVisible: !prevState.passwordVisible };
+        })
+    }
+
+    onGenerateButtonClick(event: React.MouseEvent<HTMLInputElement>) {
+        let website: string = this.state.website ? this.state.website : '', password: string = this.state.password ? this.state.password : '', accountId: string = this.state.accountId ? this.state.accountId : '';
+        let salt: string = website + accountId;
+        let wordArray: CryptoJS.lib.WordArray = CryptoJS.PBKDF2(password, salt, {
+            keySize: this.state.digitNumber, iterations: 100, hasher: HASHER[this.state.hashType]
+        });
+        let result: string = wordArray.words.map((v: number, i: number) => {
+            if (this.state.digitNumber === undefined || i >= this.state.digitNumber) return;
+            let index: number = v % ALPHABET.length;
+            index < 0 && (index += ALPHABET.length);
+            return ALPHABET[index];
+        }).join('');
+        this.setState({ genPassword: result });
     }
 }
---
+++ src/lib.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="../node_modules/@types/cryptojs/index.d.ts" />
+declare module "crypto-js" {
+    export = CryptoJS;
+}
